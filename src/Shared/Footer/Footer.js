1fce6fdb66709ee466a5dff9c31f14fa24ce2267
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-10-07 15:58:17 +0600

    created header and footer

---
+++ src/Shared/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer items-center p-4 bg-neutral text-neutral-content mb-0">
+            <p className='text-center'>Copyright © 2022 - All right reserved</p>
+        </footer>
+    );
+};
+
+export default Footer;
