2f16ccd90de87041a987a233955397d820ed5489
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-10-07 22:17:06 +0600

    added pagination

--- src/Components/AllMovies.js
+++ src/Components/AllMovies.js
@@ -3,21 +3,40 @@ import MoviesCards from './MoviesCards';
 
 const AllMovies = () => {
     const [movies, setMovies] = useState([]);
+    const [page, setPage] = useState(1)
 
     useEffect(()=>{
-        fetch('https://movie-task.vercel.app/api/popular?page=1')
+        fetch(`https://movie-task.vercel.app/api/popular?page=${page}`)
         .then(res => res.json())
         .then(data => {
             console.log(data.data.results);
              setMovies(data.data.results)})
-    },[])
+    },[page])
+
+    const handlePrev = () =>{
+        if( page > 1){
+           setPage(page - 1)
+        }
+
+
+    };
+
+    const handleNext = () =>{
+        setPage( page + 1)
+    };
+    
     return (
-        <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-12'>
-
-            {
-                movies.map(movie => <MoviesCards key={movie.id} movie={movie}></MoviesCards>)
-            }
-
+        <div>
+            <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-12'>
+                 {
+                     movies.map(movie => <MoviesCards key={movie.id} movie={movie}></MoviesCards>)
+                 }
+            </div>
+            <div className="btn-group mt-4">
+              <button onClick={handlePrev} className="btn lowercase">«previous</button>
+              <button className="btn">Page {page}</button>
+              <button onClick={handleNext} className="btn lowercase px-8">next»</button>
+            </div>
         </div>
     );
 };
