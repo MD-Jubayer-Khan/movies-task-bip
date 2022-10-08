a0d6942a84215d608b9d89be9942d990cf44cbf1
MD-Jubayer-Khan <mdjubayerkhan001@gmail.com>
2022-10-08 13:20:55 +0600

    created filter and search element

--- src/Components/AllMovies.js
+++ src/Components/AllMovies.js
@@ -32,6 +32,28 @@ const AllMovies = () => {
     
     return (
         <div>
+            <div className='flex place-content-between'>
+          <div className='flex'>
+          <select  className=" bg-transparent border border-secondary rounded w-40 ml-20 mb-4 my-2 pl-3 pr-4 font-bold">
+            <option className='bg-transparent ' disabled selected>Filter by Year</option>
+              {/* {category.map(p => <option key={p.id} >{p.category}</option>)} */}
+          </select>
+          <div  className='flex ml-3'>
+           <p className='text-xl font-bold mt-4 ml-2 text-primary'>Reset</p>
+          </div>
+        
+          </div>
+        
+            <div className='flex'>
+            <div className="form-control mr-20 my-3">
+                <form className='flex'>
+                     <input   type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />    
+                     <input className='btn btn-outline ml-3 px-8' type="submit" value="Search" />                
+                </form>
+
+            </div>
+            </div>
+          </div>
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-12'>
                  {
                      movies.map(movie => <MoviesCards key={movie.id} movie={movie}></MoviesCards>)
